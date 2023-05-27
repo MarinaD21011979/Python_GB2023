7f1e9a0600090363f8b44362841a14f0b1c1e8a8
Marina <m.a.dedova@gmail.com>
2023-05-27 23:52:13 +0300

    set

---
+++ lesson2_Lists/# множества (sets).py
@@ -0,0 +1,14 @@
+# множества (sets)
+colors = {'green', 'red', 'blue', 'black'}
+print(colors) 
+colors.add('brown')
+print(colors)
+colors.add('brown')
+print(colors)
+colors.remove('blue')
+print(colors)
+#colors.remove('blue')
+colors.discard('blue') # checking is there a value
+print(colors)
+colors.clear() #delete all elements
+print(colors) # print empty set
