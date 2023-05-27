c196085e6faf985a3b6231644ec05e077d6647bb
Marina <m.a.dedova@gmail.com>
2023-05-27 22:10:22 +0300

    insert

--- lesson2_Lists/# main functions in lists.py
+++ lesson2_Lists/# main functions in lists.py
@@ -14,3 +14,10 @@ print(list_1)
 list_1 = [12, 7, -1, 21, 0]
 print(list_1.pop(0)) # to delete element with specific index, 0 in this case
 print(list_1)
+print(list_1.pop(3))
+print(list_1)
+
+# to add the element to the list for the needed position - function insert
+list_1 = [12, 7, -1, 21, 0]
+print(list_1.insert(5, 9)) # 2 arguments: 1 - position = index, 2 - value = to add 9 on the position with index 5
+print(list_1)
