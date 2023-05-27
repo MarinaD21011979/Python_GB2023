15a18b4cfa9ed7f52caa43351f474e026a75f2ea
Marina <m.a.dedova@gmail.com>
2023-05-27 21:18:37 +0300

    changes1

--- lesson2_Lists/# lesson2.py
+++ lesson2_Lists/# lesson2.py
@@ -2,4 +2,8 @@
 list_1 = []
 list_1 = list()
 list_1 = [1, 2, 3, 4, 5, 8]
-print(list_1)
+print(*list_1) # вывод без квадратных скобок
+
+# for i in list_1:
+    # print(i)
+print(len(list_1)) # подсчет эдементов
