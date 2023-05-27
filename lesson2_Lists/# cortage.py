c90db562541a7086f0b1e24b36ed5f4034078d07
Marina <m.a.dedova@gmail.com>
2023-05-27 22:58:33 +0300

    end

--- lesson2_Lists/# cortage.py
+++ lesson2_Lists/# cortage.py
@@ -4,5 +4,19 @@ t = ()
 
 print(type(t))
 
-t = (1)
+t = (1, )
 print(type(t))
+
+v = [1, 2, 3 ]
+print(v)
+print(type(v))
+
+v = tuple(v)
+print(v)
+print(type(v)) # transform to cortage
+
+# a,b = 1,2 # множественное присваивание a = b = 1
+# divide the cortage to three variable
+
+a,b,c = v  
+print(a, b, c)
