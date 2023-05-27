72e394c2885e417c19a1530d8db993321a69f7f9
Marina <m.a.dedova@gmail.com>
2023-05-27 21:24:47 +0300

    end

--- lesson2_Lists/# lesson2.py
+++ lesson2_Lists/# lesson2.py
@@ -1,4 +1,4 @@
-# lesson2
+# lesson2 to create empty list and list with values
 list_1 = []
 list_1 = list()
 list_1 = [1, 2, 3, 4, 5, 8]
@@ -9,3 +9,4 @@ print(*list_1) # вывод без квадратных скобок
 print(len(list_1)) # подсчет эдементов
 
 print(list_1[3]) # указываем индекс элемента, к которому обращаемся
+print(list_1[-1])
