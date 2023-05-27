44086c5e80e0480c54fb5521ed08c756c2c16204
Marina <m.a.dedova@gmail.com>
2023-05-27 22:02:30 +0300

    pop

--- lesson2_Lists/# list and cycle.py
+++ lesson2_Lists/# list and cycle.py
@@ -1,6 +1,7 @@
 # list and cycle
 
 list_1 = []
+print(list_1)
 for i in range(5):
     list_1.append(i) # i will take value from 0 till 4
     print(list_1)
---
+++ lesson2_Lists/# main functions in lists.py
@@ -0,0 +1,16 @@
+# main functions in lists
+# 1 to delete the last element - pop 
+list_1 = [12, 7, -1, 21, 0]
+a = list_1.pop()
+print(a) # delete the last element
+print(list_1.pop()) #0
+print(list_1)
+print(list_1.pop()) #21
+print(list_1)
+print(list_1.pop()) # -1
+print(list_1)
+
+# to delete the specific element from the list
+list_1 = [12, 7, -1, 21, 0]
+print(list_1.pop(0)) # to delete element with specific index, 0 in this case
+print(list_1)
